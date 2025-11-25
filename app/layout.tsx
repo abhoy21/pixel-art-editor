f8960bbe06ddc297c938c3809039a4a63261796e
Abhoy Sarkar <sarkar.ab07@gmail.com>
2025-11-25 20:04:38 +0530

    init

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/next'
+import { ThemeProvider } from '@/components/theme-provider'
+import './globals.css'
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: 'pixel-art-editor App',
+  description: 'Created with',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className} antialiased`}>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+          {children}
+        </ThemeProvider>
+        <Analytics />
+      </body>
+    </html>
+  )
+}
